c219d382a04792892de40cac2228b15fc0a3ac61
Shivenraghav2001 <EE24MT030@iitdh.ac.in>
2024-10-31 04:41:28 +0530

    defining I2C0_Init function

--- main.c
+++ main.c
@@ -13,3 +13,18 @@
 
 #define MCP4725_ADDRESS 0x60                              // I2C Address for MCP4725 DAC
 
+void I2C0_init(void) {
+    SYSCTL_RCGCI2C_R |= 0x01;                             // Enable I2C0 clock
+    SYSCTL_RCGCGPIO_R |= 0x01;                            // Enable GPIO Port A clock
+
+    while ((SYSCTL_PRGPIO_R & 0x01) == 0);                // Wait for Port A clock to stabilize
+
+    GPIO_PORTA_AFSEL_R |= 0xC0;                           // Enable alternate function on PA6 (SCL) and PA7 (SDA)
+    GPIO_PORTA_ODR_R |= 0x80;                             // Enable open-drain on PA7 (SDA)
+    GPIO_PORTA_DEN_R |= 0xC0;                             // Enable digital function on PA6 and PA7
+    GPIO_PORTA_PCTL_R &= ~0xFF000000;                     // Clear PCTL for PA6 and PA7
+    GPIO_PORTA_PCTL_R |= 0x33000000;                      // Set I2C function for PA6 and PA7
+
+    I2C0_MCR_R = 0x10;                                    // Initialize I2C0 master function
+    I2C0_MTPR_R = 0x07;                                   // Set SCL clock frequency (based on 100kHz standard mode)
+}
