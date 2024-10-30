75ae7370002aa43d592f5529d532428b78622a38
rahul <ee24mt007@iitdh.ac.in>
2024-10-31 04:31:54 +0530

    Initial

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <math.h>
+#include "tm4c123gh6pm.h"
+
+#define STCTRL *((volatile uint32_t *) 0xE000E010)        // STATUS AND CONTROL REGISTER
+#define STRELOAD *((volatile uint32_t *) 0xE000E014)      // RELOAD VALUE REGISTER
+#define STCURRENT *((volatile uint32_t *) 0xE000E018)     // CURRENT VALUE REGISTER
+
+#define ENABLE (1 << 0)                                   // ENABLE TIMER BY SETTING BIT 0 OF CSR
+#define CLKINT (1 << 2)                                   // SPECIFY CPU CLOCK
+#define CLOCK_HZ 16000000                                 // CLOCK FREQUENCY
+#define SYSTICK_RELOAD_VALUE(us) ((CLOCK_HZ / 1000000) * (us) - 1)   // SYSTICK RELOAD VALUE CONVERSION IN MICROSECONDS
+
+#define MCP4725_ADDRESS 0x60                              // I2C Address for MCP4725 DAC
+
